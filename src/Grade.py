f926d978b77af43d88ce0bdc27a3bbf3db3b3ffc
Jacob Abramson <jacob@jabramson.com>
2015-06-26 23:51:15 -0400

    Add grade class, add basic curve/file reading functionality

---
+++ src/Grade.py
@@ -0,0 +1,19 @@
+'''
+    Filename: Grade.py
+    Author: Jacob Abramson
+    Date created: 6/26/2015
+'''
+
+class Grade:
+
+    def __init__(self, g):
+        self.origGrade = int(g)
+
+    def setCurvedGrade(self, g):
+        self.curvedGrade = g
+
+    def getOrigGrade(self):
+        return self.origGrade
+
+    def getCurvedGrade(self):
+        return self.curvedGrade
--- src/main.py
+++ src/main.py
@@ -4,6 +4,11 @@
     Date created: 6/26/2015
 '''
 
+from Grade import Grade
+
+# list of grade objects
+gradesList = []
+
 # introduce the user
 def start():
     print("~~~~~~~~~~~ Welcome to Grade Curver! ~~~~~~~~~~~~")
@@ -16,7 +21,60 @@ def start():
 
 # help method
 def _help():
-    print("Commands:")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~ Help ~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("To begin, please enter in a list of grades in grades.txt")
+    print("Format:")
+    print("96\n72\n85\n...")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+# calculate curve
+def calc():
+
+    if isProcessed() is False:
+        process()
+
+    # curve based on how far highest grade was from 100
+    # (so if highest grade is 82, add 18 points to everyone's grade)
+    maxGrade = 0
+    for g in gradesList:
+        if g.getOrigGrade() > maxGrade:
+            maxGrade = g.getOrigGrade()
+
+    incGrade = 100 - maxGrade
+    for g in gradesList:
+        g.setCurvedGrade(g.getOrigGrade() + incGrade)
+
+    display()
+
+def process():
+
+    origGradesFile = open("grades.txt")
+    lines = origGradesFile.readlines()
+
+    for line in lines:
+        newGrade = Grade(int(line.rstrip()))
+        gradesList.append(newGrade)
+
+    origGradesFile.close()
+
+def isProcessed():
+    return False
+
+def displayOrigGrades():
+
+    print("Original grades:")
+    for g in gradesList:
+        print(g.getOrigGrade())
+
+def displayCurvedGrades():
+
+    print("Curved grades:")
+    for g in gradesList:
+        print(g.getCurvedGrade())
+
+def display():
+    displayOrigGrades()
+    displayCurvedGrades()
 
 # main loop to interface with the user
 def loop():
@@ -28,7 +86,10 @@ def loop():
 
         if command == "help":
             _help()
-            
+        elif command == "calc":
+            calc()
+        elif command == "display":
+            display()
 
 # begin execution of the application
 start()
